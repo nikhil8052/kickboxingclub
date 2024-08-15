e66cdceb32856555996054a8be360d16589a2af4
Your Name <you@example.com>
2024-08-15 05:13:25 +0000

    updated

--- app/Models/MembershipTransaction.php
+++ app/Models/MembershipTransaction.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class MembershipTransaction extends Model
 {
     use HasFactory;
+
+    public function membership_instance(){
+        return $this->hasOne(MembershipInstances::class,'membership_id','membership_instances_id');
+    }
 }

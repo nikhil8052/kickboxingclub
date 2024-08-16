f20c7980ff8ad97e94834eab483f2755d5561677
Your Name <you@example.com>
2024-08-16 13:43:55 +0000

    updated

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -13,6 +13,7 @@ use App\Models\MembershipInstances;
 use Carbon\Carbon;
 use App\Models\AllUsers;
 use App\Models\Employees;
+use App\Models\OrderLine;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
@@ -21,8 +22,95 @@ class OrdersController extends Controller
 
     public function Dashboard(Request $request)
     {
-        $Totalsalessum = Orders::where('status', 'completed')->sum('total');
-        $totalsales = formatCurrency($Totalsalessum);
+        $TotalsalesCompleted = Orders::where('status', 'completed')->orWhere('status','Partially Refunded')->sum('total');
+        $TotalsalesPR = Orders::Where('status','Partially Refunded')->sum('total_amount_refunded');
+        // $totaloverAllsales = formatCurrency($TotalsalesCompleted -  $TotalsalesPR );
+        // dd($Totalsalessum);
+
+        $GetORder = OrderLine::where('status', 'completed')->orWhere('status','Partially Refunded')->sum('line_total');
+        $GetORderunit = OrderLine::Where('status','Partially Refunded')->get();
+        $GetORderunitamount = 0;
+        foreach($GetORderunit as $unit) {
+            $TotalsalesPR = Orders::Where('order_id',$unit->order_id)->first();
+            if($TotalsalesPR) {
+                $GetORderunitamount += $TotalsalesPR->total_amount_refunded;
+            }
+        }
+
+        $totaloverAllsales = formatCurrency($GetORder - $GetORderunitamount);
+
+
+       
+        $orderswL = Orders::with('orderlines')->get();
+        $totalsale =0;
+        $totalmembSale = 0 ;
+        $totalcreditsale =0;
+        $totalRefundedAmount = 0;
+        $totalRefundedAmountC = 0;
+
+        foreach ($orderswL as $order) {
+        
+            foreach ($order->orderlines as $orderline) {
+                if($orderline->transaction_type  == 'MembershipTransaction' && $orderline->status == 'Completed' ){
+                    $totalmembSale += $orderline->line_total;
+                }
+    
+                if($orderline->transaction_type  == 'MembershipTransaction' && $orderline->status == 'Partially Refunded' ){
+                    $orderdata = Orders::Where('order_id',$orderline->order_id)->first();
+                    $totalRefundedAmount += $orderdata->total_amount_refunded;
+                    $totalmembSale += $orderline->line_total;
+                }
+        
+        
+                if( $orderline->transaction_type  == 'CreditTransaction' && $orderline->status == 'Completed' ){
+                    $totalcreditsale += $orderline->line_total;
+                }
+                if($orderline->transaction_type  == 'CreditTransaction' && $orderline->status == 'Partially Refunded' ){
+                    $orderdataC = Orders::Where('order_id',$orderline->order_id)->first();
+                    $totalRefundedAmountC += $orderdataC->total_amount_refunded;
+                    $totalcreditsale += $orderline->line_total;
+                }
+            }
+        }
+       
+        // foreach($orderswL as $order){
+        //     if($order->orderline->transaction_type  == 'MembershipTransaction' && $order->orderline->status == 'Completed' ){
+        //         $totalmembSale += $order->orderline->line_total;
+        //     }
+
+        //     if($order->orderline->transaction_type  == 'MembershipTransaction' && $order->orderline->status == 'Partially Refunded' ){
+        //         $orderdata = Orders::Where('order_id',$order->orderline->order_id)->first();
+        //         $totalRefundedAmount += $orderdata->total_amount_refunded;
+        //         $totalmembSale += $order->orderline->line_total;
+        //     }
+
+        // }
+
+        // foreach($orderswL as $order){
+          
+        //     if($order->orderline->status == 'Completed' ){
+        //         $totalsale += $order->orderline->line_total;
+        //     }
+        // }
+
+
+        // foreach($orderswL as $order){
+        //     if( $order->orderline->transaction_type  == 'CreditTransaction' && $order->orderline->status == 'Completed' ){
+        //         $totalcreditsale += $order->orderline->line_total;
+        //     }
+        //     if($order->orderline->transaction_type  == 'CreditTransaction' && $order->orderline->status == 'Partially Refunded' ){
+        //         $orderdataC = Orders::Where('order_id',$order->orderline->order_id)->first();
+        //         $totalRefundedAmountC += $orderdataC->total_amount_refunded;
+        //         $totalcreditsale += $order->orderline->line_total;
+        //     }
+        // }
+       
+        // $order_line = OrderLine::where('status', 'Completed')->sum('line_total');
+        // dd($order_line);
+      
+        $totalsales = formatCurrency($totalsale);
+        $totalcreditSales = formatCurrency($totalcreditsale - $totalRefundedAmountC);
+        $totalMembershipSales = formatCurrency($totalmembSale - $totalRefundedAmount);
 
         $forecastrecord = MembershipInstances::sum('renewal_rate');
         $forecastsales = formatCurrency($forecastrecord);
@@ -51,6 +139,12 @@ class OrdersController extends Controller
         $TrialSoldMemberships = MembershipInstances::query();
         $visitorsdata = AllUsers::query();
 
+        $totalMembershipSalesR = MembershipInstances::whereIn('status', ['active', 'cancelled', 'terminated', 'done', 'frozen'])
+            ->selectRaw('SUM(renewal_rate * renewal_count) as total_sales')
+            ->first();
+        // $totalMembershipSales = formatCurrency($totalMembershipSalesR['total_sales']);
+
+        // dd($totalMembershipSales);
         if ($startDate && $endDate) {
             $queryOrders->whereBetween(DB::raw('STR_TO_DATE(date_placed, "%Y-%m-%d")'), [$startDate, $endDate]);
             $queryMemberships->whereBetween('purchase_date_copy', [$startDate, $endDate]);
@@ -116,7 +210,10 @@ class OrdersController extends Controller
                 'cancelledMemberships' => $cancelledMemberships,
                 'TrialSoldMembershipsCount' => $TrialSoldMembershipsCount,
                 'allvisitors' => $allvisitors,
-                'locations' => $locations
+                'locations' => $locations,
+                'totalMembershipSales' => $totalMembershipSales,
+                'totalcreditSales' => $totalcreditSales,
+                'totaloverAllsales' => $totaloverAllsales
             ]);
         }
 
@@ -132,6 +229,9 @@ class OrdersController extends Controller
             'TrialSoldMembershipsCount' => $TrialSoldMembershipsCount,
             'allvisitors' => $allvisitors,
             'locations' => $locations,
+            'totalMembershipSales' => $totalMembershipSales,
+            'totalcreditSales' => $totalcreditSales,
+            'totaloverAllsales' => $totaloverAllsales
         ]);
     }
 
@@ -186,7 +286,58 @@ class OrdersController extends Controller
 
     public function Orders()
     {
-        return view('admin_dashboard.orders.orders2');
+        $query = Orders::query();
+
+        $locations = Locations::all();
+    
+
+        $totalcompletedSale = 0 ;
+        $totalRefunds =0;
+        $totalRefundedAmountPartiall = 0;
+        $totalcancelled = 0;
+        $totalPending = 0;
+        $totalPF = 0;
+        $orderswL = $query->with('orderlines')->get();
+        foreach ($orderswL as $order) {
+        
+            foreach ($order->orderlines as $orderline) {
+                if( $orderline->status == 'Completed' ){
+                    $totalcompletedSale += $orderline->line_total;
+                }
+    
+                if($orderline->status == 'Partially Refunded' ){
+                    $orderdata = Orders::Where('order_id',$orderline->order_id)->first();
+                    $totalRefundedAmountPartiall += $orderdata->total_amount_refunded;
+                    $totalcompletedSale += $orderline->line_total;
+                }
+                if( $orderline->status == 'Refunded' ){
+                    $orderdata = Orders::Where('order_id',$orderline->order_id)->first();
+                    $totalRefunds += $orderdata->total_amount_refunded;
+                }
+                if($orderline->status == 'Cancelled' ){
+                    $totalcancelled += $orderline->line_total;
+                }
+
+                if($orderline->status == 'Payment Failure'){
+                    $totalPF += $orderline->line_total;
+                }
+
+                if($orderline->status == 'Pending' || $orderline->status == 'Deferred'  ){
+                    $totalPending += $orderline->line_total;
+                }
+            }
+        }
+
+        $masterArray = [
+            'totalcompletedSale' => formatCurrency($totalcompletedSale - $totalRefundedAmountPartiall),
+            'totalRefunds' => formatCurrency($totalRefunds + $totalRefundedAmountPartiall),
+            'totalPending'  => formatCurrency($totalPending),
+            'totalcancelled' => formatCurrency($totalcancelled),
+            'totalPF'  => formatCurrency($totalPF)
+
+        ]; 
+
+        return view('admin_dashboard.orders.orders2',compact('locations','masterArray'));
     }
 
     public function SalesStats()
@@ -199,152 +350,73 @@ class OrdersController extends Controller
     {
         $startDate = $request->startDate;
         $endDate = $request->endDate;
+        $location = $request->location;
+
+        // $current_location = Locations::where('location_id',$location_id)->first();
     
         $query = Orders::query();
 
         $locations = Locations::all();
     
         if ($startDate && $endDate) {
-            $query->whereBetween(DB::raw('STR_TO_DATE(date_placed, "%Y-%m-%d")'), [$startDate, $endDate]);
+            $query->whereBetween(DB::raw('STR_TO_DATE(date_placed, "%Y-%m-%d")'), [Carbon::parse($startDate), Carbon::parse($endDate)]);
         }
-    
-        $alldataSums = $query->where('status', 'completed')
-            ->select('location')
-            ->selectRaw('SUM(total) as total')
-            ->groupBy('location')
-            ->get()
-            ->keyBy('location')
-            ->mapWithKeys(fn($item) => [$item->location => round($item->total)]);
 
-        $query = Orders::query();
-        if ($startDate && $endDate) {
-            $query->whereBetween(DB::raw('STR_TO_DATE(date_placed, "%Y-%m-%d")'), [$startDate, $endDate]);
+        if($location) {
+            $query->whereBetween('location',$location);
         }
+
+        $totalcompletedSale = 0 ;
+        $totalRefunds =0;
+        $totalRefundedAmountPartiall = 0;
+        $totalcancelled = 0;
+        $totalPending = 0;
+        $totalPF = 0;
+        $orderswL = $query->with('orderlines')->get();
+        foreach ($orderswL as $order) {
+        
+            foreach ($order->orderlines as $orderline) {
+                if( $orderline->status == 'Completed' ){
+                    $totalcompletedSale += $orderline->line_total;
+                }
     
-        $FailedPayments = $query->where('status', 'Payment Failure')
-            ->select('location')
-            ->selectRaw('COUNT(*) as count')
-            ->groupBy('location')
-            ->get()
-            ->keyBy('location')
-            ->mapWithKeys(fn($item) => [$item->location => $item->count]);
-    
-        $query = Orders::query();
-        if ($startDate && $endDate) {
-            $query->whereBetween(DB::raw('STR_TO_DATE(date_placed, "%Y-%m-%d")'), [$startDate, $endDate]);
-        }
-    
-        $CompletePayments = $query->where('status', 'completed')
-            ->select('location')
-            ->selectRaw('COUNT(*) as count')
-            ->groupBy('location')
-            ->get()
-            ->keyBy('location')
-            ->mapWithKeys(fn($item) => [$item->location => $item->count]);
-    
-        $query = Orders::query();
-        if ($startDate && $endDate) {
-            $query->whereBetween(DB::raw('STR_TO_DATE(date_placed, "%Y-%m-%d")'), [$startDate, $endDate]);
-        }
-    
-        $PendingPayments = $query->where('status', 'Pending')
-            ->select('location')
-            ->selectRaw('COUNT(*) as count')
-            ->groupBy('location')
-            ->get()
-            ->keyBy('location')
-            ->mapWithKeys(fn($item) => [$item->location => $item->count]);
-    
-        $query = MembershipInstances::query();
-        if ($startDate && $endDate) {
-            $query->whereBetween(DB::raw('STR_TO_DATE(purchase_date, "%Y-%m-%d")'), [$startDate, $endDate]);
-        }
-    
-        $forecastrecord = $query->groupBy('purchase_location_id')
-            ->select('purchase_location_id', DB::raw('SUM(renewal_rate) as total_renewal_rate'))
-            ->get()
-            ->pluck('total_renewal_rate', 'purchase_location_id')
-            ->mapWithKeys(function ($sum, $locationId) {
-                $locationName = Locations::find($locationId)->name;
-                return [$locationName => $sum];
-            });
-    
-        $query = MembershipInstances::query();
-        if ($startDate && $endDate) {
-            $query->whereBetween(DB::raw('STR_TO_DATE(purchase_date, "%Y-%m-%d")'), [$startDate, $endDate]);
-        }
-    
-        $activeMembershiprecord = $query->where('status', 'active')
-            ->pluck('purchase_location_id')
-            ->filter()
-            ->countBy();
-        $activeMembership = Locations::whereIn('location_id', $activeMembershiprecord->keys())
-            ->pluck('name', 'location_id')
-            ->mapWithKeys(fn($name, $id) => [$name => $activeMembershiprecord[$id]]);
-    
-        $query = MembershipInstances::query();
-        if ($startDate && $endDate) {
-            $query->whereBetween(DB::raw('STR_TO_DATE(purchase_date, "%Y-%m-%d")'), [$startDate, $endDate]);
-        }
-    
-        $cancelledMembershiprecord = $query->whereIn('status', ['cancelled', 'terminated'])
-            ->pluck('purchase_location_id')
-            ->filter()
-            ->countBy();
-        $cancelledMembership = Locations::whereIn('location_id', $cancelledMembershiprecord->keys())
-            ->pluck('name', 'location_id')
-            ->mapWithKeys(fn($name, $id) => [$name => $cancelledMembershiprecord[$id]]);
-    
-        $masterArray = [];
-    
-        foreach ($locations as $location) {
-            if (!isset($masterArray[$location->name])) {
-                $masterArray[$location->name] = [
-                    'alldataSum' => 0,
-                    'failedPayments' => 0,
-                    'completePayments' => 0,
-                    'pendingPayments' => 0,
-                    'forecast' => 0,
-                    'activeMembership' => 0,
-                    'cancelledMembership' => 0,
-                ];
+                if($orderline->status == 'Partially Refunded' ){
+                    $orderdata = Orders::Where('order_id',$orderline->order_id)->first();
+                    $totalRefundedAmountPartiall += $orderdata->total_amount_refunded;
+                    $totalcompletedSale += $orderline->line_total;
+                }
+                if( $orderline->status == 'Refunded' ){
+                    $orderdata = Orders::Where('order_id',$orderline->order_id)->first();
+                    $totalRefunds += $orderdata->total_amount_refunded;
+                }
+                if($orderline->status == 'Cancelled' ){
+                    $totalcancelled += $orderline->line_total;
+                }
+
+                if($orderline->status == 'Payment Failure'){
+                    $totalPF += $orderline->line_total;
+                }
+
+                if($orderline->status == 'Pending' || $orderline->status == 'Deferred'  ){
+                    $totalPending += $orderline->line_total;
+                }
             }
         }
-    
-        foreach ($alldataSums as $location => $total) {
-            $masterArray[$location]['alldataSum'] = formatCurrency($total) ;
-        
-        }
 
-        foreach ($FailedPayments as $location => $count) {
-            $masterArray[$location]['failedPayments'] = formatCurrency($count);
-        }
-    
-        foreach ($CompletePayments as $location => $count) {
-            $masterArray[$location]['completePayments'] =  formatCurrency($count);
-        }
-    
-        foreach ($PendingPayments as $location => $count) {
-            $masterArray[$location]['pendingPayments'] =  formatCurrency($count);
-        }
-    
-        foreach ($forecastrecord as $location => $total_renewal_rate) {
-            $masterArray[$location]['forecast'] =  formatCurrency($total_renewal_rate);
-        }
-    
-        foreach ($activeMembership as $location => $count) {
-            $masterArray[$location]['activeMembership'] = formatCurrency($count);
-        }
-    
-        foreach ($cancelledMembership as $location => $count) {
-            $masterArray[$location]['cancelledMembership'] = formatCurrency($count);
-        }
+        $masterArray = [
+            'totalcompletedSale' => formatCurrency($totalcompletedSale - $totalRefundedAmountPartiall),
+            'totalRefunds' => formatCurrency($totalRefunds + $totalRefundedAmountPartiall),
+            'totalPending'  => formatCurrency($totalPending),
+            'totalcancelled' => formatCurrency($totalcancelled),
+            'totalPF'  => formatCurrency($totalPF)
+
+        ]; 
     
         return response()->json(['masterArray' => $masterArray]);
     }
     
-    function formatCurrency($amount) {
-        $amount = number_format($amount, 2, '.', '');
+    function formatCurrenc_y($amount) {
+        $amount = number_format($amount, 2, '.', ',');
         $parts = explode('.', $amount);
         $rupees = $parts[0];
         $paise = isset($parts[1]) ? $parts[1] : '00';
@@ -357,10 +429,26 @@ class OrdersController extends Controller
         if(strlen($rupees) === 5){
             $rupees = preg_replace('/\B(?=(\d{3})+(?!\d))/', ',', $rupees);
             // $rupees = preg_replace('/\B(?=(\d{2})+(?=\d{3}))/u',',', $rupees);
-        }
+        } 
         
         return $rupees . '.' . $paise;
     } 
+    function formatCurrency($amount) {
+        // Remove any existing formatting
+        $amount = str_replace(',', '', $amount);
+        
+        // Split the number into the integer and decimal parts
+        $parts = explode('.', $amount);
+        $rupees = $parts[0];
+        $paise = isset($parts[1]) ? $parts[1] : '00';
+        
+        // Add commas in standard thousand separator format
+        $formattedRupees = preg_replace('/\B(?=(\d{3})+(?!\d))/', ',', $rupees);
+    
+        // Return the formatted amount with two decimal places
+        return $formattedRupees . '.' . str_pad($paise, 2, '0');
+    }
+    
     
     public function GetOrders(Request $request)
     {

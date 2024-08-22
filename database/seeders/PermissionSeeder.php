3e467db887b74e76096df50d23a7a1499d506f4f
Your Name <you@example.com>
2024-08-22 13:57:09 +0000

    new changes

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -4,14 +4,36 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
 
 class PermissionSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
+    
     public function run(): void
     {
-        //
+
+        DB::table('permissions')->delete();
+        
+        $permissions = [
+            'memberships',
+            'payrolls',
+            'orders',
+            'members',
+            'billing',
+            'employees'
+        ];
+
+        // Insert the permissions into the database
+        foreach ($permissions as $permission) {
+            DB::table('permissions')->insert([
+                'permission' => $permission,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
     }
 }
